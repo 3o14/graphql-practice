aee2e53b3dd46ef34601d1ff8f3d7dad0de10185
3o14 <dldnjswn4265@gmail.com>
2023-02-01 03:02:07 +0900

    Mutation Resolvers

---
+++ server.js
@@ -0,0 +1,74 @@
+import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer, gql } from "apollo-server";
+
+let tweets = [
+  {
+    id: "1",
+    text: "first one!",
+  },
+  {
+    id: "2",
+    text: "second one",
+  },
+];
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    username: String!
+    firstName: String!
+    lastName: String
+  }
+  type Tweet {
+    id: ID!
+    text: String!
+    author: User
+  }
+  // Query는 GET request라면 mutation은 POST, DELETE request
+  type Query {
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+  }
+  // Mutation은 API와 통신할 수 있게 해줌 (post, delete 등)
+  type Mutation { 
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
+  }
+`;
+// GET /api/v1/tweets
+// POST DELETE PUT /api/v1/tweets
+// GET /api/v1/tweet/:id
+
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
+      if (!tweet) return false;
+      tweets = tweets.filter((tweet) => tweet.id !== id);
+      return true;
+    },
+  },
+};
+
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Running on ${url}`);
+});
